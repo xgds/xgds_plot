ffccdb117e0e50d754275c62eb0a59afdb16bec6
Trey Smith <trey.smith@nasa.gov>
2012-07-05 10:36:40 -0700

    improved flexibility of settings.XGDS_PLOT_TIME_SERIES format

--- xgds_plot/defaultSettings.py
+++ xgds_plot/defaultSettings.py
@@ -6,5 +6,64 @@
 
 XGDS_ZMQ_WEB_SOCKET_URL = 'ws://{{host}}:8001/zmq/'
 
-# A list of data sets available for plotting.
+# Specifies which time series are available for plotting. See example below.
 XGDS_PLOT_TIME_SERIES = ()
+
+"""
+
+There are four types of time series meta-data records, in the following
+'inheritance hierarchy':
+
+ * TimeSeriesMeta
+   * Group
+   * TimeSeries
+   * IncludeMeta
+   * IncludeFunctionResultMeta
+
+Ignoring the IncludeX types for the moment, the XGDS_PLOT_TIME_SERIES
+object is organized as a tree whose internal nodes are Groups and whose
+leaves are TimeSeries. The top-level XGDS_PLOT_TIME_SERIES object must
+be a Group.
+
+The IncludeX types provide a way to include more time series meta-data
+specified in other Python modules. IncludeX objects are evaluated and
+interpolated into the tree structure at startup time, so at run-time the
+tree contains only Group and TimeSeries objects. (The fully evaluated
+tree is also what gets passed to the JavaScript client side.)
+
+IncludeX objects must be contained in the 'members' list of a Group
+object. Each IncludeX object must evaluate to a TimeSeriesMetaList, that
+is, an iterable of TimeSeriesMeta objects, which is interpolated into
+the members list.
+
+Each IncludeX object has a 'name' field which is a fully-qualified
+Python identifier, including the module name. Each IncludeMeta object
+evaluates to the value of its 'name' identifier. Each
+IncludeFunctionResultMeta object treats the value of its 'name'
+identifier as a Python callable and evaluates to the result of invoking
+the callable. Evaluation is recursive, so IncludeX results can include
+other IncludeX objects.
+
+XGDS_PLOT_TIME_SERIES = {
+    'type': 'Group',
+    'name': 'All',
+    'members': [
+        {
+            'type': 'TimeSeries',
+            'valueField': 'snScalar',
+            'queryModel': 'isruApp.resolvenstier1data',
+            'queryTimestampField': 'timestampSeconds',
+            'show': True,
+        },
+        {
+            'type': 'IncludeFunctionResultMeta',
+            'name': 'isruApp.views.getNirvsBandDepths',
+        },
+        {
+            'type': 'IncludeMeta',
+            'name': 'isruApp.settings.ROVER_PARAMETER_TIME_SERIES',
+        },
+    ]
+}
+
+"""
--- xgds_plot/views.py
+++ xgds_plot/views.py
@@ -4,6 +4,8 @@
 # All Rights Reserved.
 # __END_LICENSE__
 
+import operator
+
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, Http404
 from django.template import RequestContext
@@ -14,12 +16,56 @@ from geocamUtil.loader import getClassByName
 from xgds_plot import settings
 
 
+def expandTimeSeriesMeta(meta):
+    """
+    Evaluates IncludeMeta and IncludeFunctionResultMeta objects. Returns
+    a list of trees with Group and TimeSeries nodes.
+    """
+    mtype = meta['type']
+    if mtype == 'Group':
+        lists = [expandTimeSeriesMeta(m)
+                 for m in meta['members']]
+        return [{'type': 'Group',
+                 'members': reduce(operator.add, lists, [])}]
+    elif mtype == 'TimeSeries':
+        return [meta.copy()]
+    elif mtype == 'IncludeMeta':
+        return [expandTimeSeriesMeta(m)
+                for m in getClassByName(meta['name'])]
+    elif mtype == 'IncludeFunctionResultMeta':
+        func = getClassByName(meta['name'])
+        return [expandTimeSeriesMeta(m)
+                for m in func()]
+    else:
+        raise ValueError('expandTimeSeriesMeta: unknown meta type %s'
+                         % mtype)
+
+def flattenTimeSeriesMeta(group):
+    """
+    Turns a tree of Group and TimeSeries into a flattened list of
+    TimeSeries objects.
+    """
+    members = group['members']
+    result = []
+    for meta in members:
+        mtype = meta['type']
+        if mtype == 'Group':
+            result += flattenTimeSeriesMeta(meta)
+        elif mtype == 'TimeSeries':
+            result.append(meta)
+        else:
+            raise ValueError('flattenTimeSeriesMeta: unknown meta type %s'
+                             % mtype)
+    return result
+
 def setupTimeSeries():
     """
     Process the XGDS_PLOT_TIME_SERIES setting. Normalize and fill in
     default values as needed.
     """
-    for series in settings.XGDS_PLOT_TIME_SERIES:
+    tree = expandTimeSeriesMeta(settings.XGDS_PLOT_TIME_SERIES)[0]
+    metaList = flattenTimeSeriesMeta(tree)
+    for series in metaList:
         series.setdefault('queryType', 'xgds_plot.query.Django')
         series.setdefault('valueType', 'xgds_plot.value.Scalar')
         queryClass = getClassByName(series['queryType'])
@@ -27,7 +73,7 @@ def setupTimeSeries():
         valueClass = getClassByName(series['valueType'])
         valueManager = valueClass(series, queryManager)
 
-    return settings.XGDS_PLOT_TIME_SERIES
+    return metaList
 
 TIME_SERIES = setupTimeSeries()
 
