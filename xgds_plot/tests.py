0fe0a376ae9d78173b2b16d175b376931bd46867
Trey Smith <trey.smith@nasa.gov>
2013-10-17 00:06:03 -0700

    tests.py: moved tile tests here from old isruApp location

--- xgds_plot/tests.py
+++ xgds_plot/tests.py
@@ -4,12 +4,43 @@
 # All Rights Reserved.
 # __END_LICENSE__
 
+import collections
+
 from django.test import TestCase
 
+from xgds_plot.tile import getTileBounds, getTileContainingPoint, getTileContainingBounds
+from xgds_plot import settings
 
-class xgds_plotTest(TestCase):
-    """
-    Tests for xgds_plot
-    """
-    def test_xgds_plot(self):
-        pass
+
+class TileTest(TestCase):
+    def assertNearlyEqual(self, a, b, msg=None, delta=settings.XGDS_PLOT_MAP_TILE_EPS):
+        if isinstance(a, collections.Iterable):
+            for ai, bi in zip(a, b):
+                if abs(ai - bi) > delta:
+                    if not msg:
+                        msg = '%s != %s within %g delta' % (a, b, delta)
+                    raise AssertionError(msg)
+        else:
+            super(TileTest, self).assertNearlyEqual(a, b, msg=msg, delta=delta)
+
+    def test_getTileBounds(self):
+        self.assertNearlyEqual(getTileBounds(level=1, x=0, y=0),
+                               (-180, -90, 0, 90))
+        self.assertNearlyEqual(getTileBounds(level=1, x=1, y=0),
+                               (0, -90, 180, 90))
+        self.assertNearlyEqual(getTileBounds(level=3, x=2, y=2),
+                               (-90, 0, -45, 45))
+
+    def test_getTileContainingPoint(self):
+        self.assertEqual(getTileContainingPoint(level=1, lon=-160, lat=-45),
+                         (0, 0))
+        self.assertEqual(getTileContainingPoint(level=3, lon=-120, lat=-30),
+                         (1, 1))
+
+    def test_getTileContainingBounds(self):
+        self.assertEqual(getTileContainingBounds([-179, -89, -1, 89]),
+                         (1, 0, 0))
+        self.assertEqual(getTileContainingBounds([-134, -44, -91, -1]),
+                         (3, 1, 1))
+        self.assertEqual(getTileContainingBounds([-134, -44, -89, -1]),
+                         (1, 0, 0))
