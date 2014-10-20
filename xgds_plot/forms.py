c67e66a72c801e5998ad12ac1466ad90d117d588
Trey Smith <trey.smith@nasa.gov>
2014-10-20 12:03:31 -0700

    forms.py: set required=False on the special boolean fields, so you can uncheck them...

--- xgds_plot/forms.py
+++ xgds_plot/forms.py
@@ -9,9 +9,9 @@ from django import forms
 from xgds_plot import models as plotModels
 
 
-saveAsNewVersionField = forms.BooleanField(initial=True, label='Save as new version',
+saveAsNewVersionField = forms.BooleanField(initial=True, label='Save as new version', required=False,
                                           help_text='If true, save edited entry as a new record with an incremented version number (recommended during ops, to avoid confusion)')
-startIndexingAtCurrentTimeField = forms.BooleanField(initial=True, label='Start indexing at current time',
+startIndexingAtCurrentTimeField = forms.BooleanField(initial=True, label='Start indexing at current time', required=False,
                                                      help_text='If true, overwrite "Start time" field with current time in UTC; skips indexing historical data (recommended during ops, to ensure live data is indexed in a timely way)')
 
 class TimeSeriesFormWithVersionOption(forms.ModelForm):
