29704031277eb2823438b9cae4e5e570f3f93ab9
Dave Lees <david.lees@nasa.gov>
2016-10-04 16:49:06 -0700

    Commit current iPython notebook status so it stops nagging us

---
+++ xgds_plot/notebook/profile_default/shared_notebook_config.py
@@ -0,0 +1,35 @@
+#__BEGIN_LICENSE__
+# Copyright (c) 2015, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All rights reserved.
+#
+# The xGDS platform is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#__END_LICENSE__
+
+"""
+Version-controlled shared config for IPython Notebook.
+
+Shared settings specified here are normally imported in the
+non-version-controlled file ipython_notebook_config.py where you can
+override settings on a site-specific basis.
+"""
+
+from django.conf import settings
+
+# pylint: disable=E0602
+
+c = get_config()
+c.NotebookManager.notebook_dir = settings.PROJ_ROOT + 'data/notebook/'
+c.IPKernelApp.pylab = 'inline'
+
+#c.NotebookApp.base_url = '/notebook/'
+#c.NotebookApp.base_project_url = '/notebook/'
+#c.NotebookApp.base_kernel_url = '/notebook/'
---
+++ xgds_plot/notebook/profile_default/static/custom/custom.js
@@ -0,0 +1,65 @@
+//__BEGIN_LICENSE__
+// Copyright (c) 2015, United States Government, as represented by the
+// Administrator of the National Aeronautics and Space Administration.
+// All rights reserved.
+//
+// The xGDS platform is licensed under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//__END_LICENSE__
+
+// leave at least 2 line with only a star on it below, or doc generation fails
+/**
+ *
+ *
+ * Placeholder for custom user javascript
+ * mainly to be overridden in profile/static/custom/custom.js
+ * This will always be an empty file in IPython
+ *
+ * User could add any javascript in the `profile/static/custom/custom.js` file
+ * (and should create it if it does not exist).
+ * It will be executed by the ipython notebook at load time.
+ *
+ * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
+ *
+ * Example :
+ *
+ * Create a custom button in toolbar that execute `%qtconsole` in kernel
+ * and hence open a qtconsole attached to the same kernel as the current notebook
+ *
+ *    $([IPython.events]).on('app_initialized.NotebookApp', function(){
+ *        IPython.toolbar.add_buttons_group([
+ *            {
+ *                 'label'   : 'run qtconsole',
+ *                 'icon'    : 'icon-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
+ *                 'callback': function () {
+ *                     IPython.notebook.kernel.execute('%qtconsole')
+ *                 }
+ *            }
+ *            // add more button here if needed.
+ *            ]);
+ *    });
+ *
+ * Example :
+ *
+ *  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
+ *  to load custom script into the notebook.
+ *
+ *    // to load the metadata ui extension example.
+ *    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
+ *    // or
+ *    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
+ *    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
+ *
+ *
+ * @module IPython
+ * @namespace IPython
+ * @class customjs
+ * @static
+ */
