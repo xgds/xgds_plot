ac32b0bf4dd0ef5f754de4fbcf74e8d708a130da
Tamar Cohen <tamar.cohen@nasa.gov>
2017-10-12 11:59:10 -0700

    updated urls for rest

--- xgds_plot/restUrls.py
+++ xgds_plot/restUrls.py
@@ -16,17 +16,18 @@
 
 # pylint: disable=E1120
 
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 
 from django.views.generic.base import RedirectView
 from xgds_plot import views
 
-urlpatterns = [url(r'^$', RedirectView.as_view(url=reverse_lazy('xgds_plot_plots'), permanent=False), {}, name='xgds_plot_home'),
-               url(r'^plots/$', views.plots, {}, name='xgds_plot_plots'),
-               url(r'^profiles/', views.profilesPage, {}, 'xgds_plot_profiles'),
-               
-               # Including these in this order ensures that reverse will return the non-rest urls for use in our server
-               url(r'^rest/', include('xgds_plot.restUrls')),
-               url('', include('xgds_plot.restUrls')),
+urlpatterns = [url(r'^now/$', views.now, {}, name='xgds_plot_now'),
+               url(r'^meta.json$', views.getMeta, {}, name='xgds_plot_meta'),
+               url(r'^mapIndex\.kml$', views.mapIndexKml, {},'xgds_plot_mapIndexKml'),
+              #url(r'^mapLayer_(?P<layerId>[^\.]+)\.kml$', views.mapKml, {}, 'xgds_plot_mapKml'),
+               url(r'^mapTile/(?P<layerId>[^/]+)/(?P<dayCode>\d+)/(?P<level>\d+)/(?P<x>\d+)/(?P<y>\d+)\.kml$', views.mapTileKml, {}, 'xgds_plot_mapTileKml'),
+               url(r'^profile/(?P<layerId>[^\.]*)\.png$', views.profileRender, {}, 'xgds_plot_profileRender'),
+               url(r'^profile/(?P<layerId>[^\.]*)\.csv$', views.profileCsv, {}, 'xgds_plot_profileCsv'),
+               url(r'staticPlot/(?P<seriesId>[^\.]+)\.png', views.getStaticPlot, {}, 'xgds_plot_staticPlot'),
                ]
